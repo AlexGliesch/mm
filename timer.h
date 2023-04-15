2ae84cbe619e2789a5792c94c79e3e0c68ff43bf
AlexGliesch <alexgliesch@gmail.com>
2023-04-15 16:20:17 -0300

    Initial commit.

---
+++ timer.h
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <cfloat>
+
+/// @brief Time-tracking structure
+class Timer
+{
+	using clock = std::chrono::steady_clock;
+  clock::time_point tpstart{};
+  double tmlim{DBL_MAX};
+  
+public: 
+	Timer(const double tl_sec = DBL_MAX)
+	{
+		Reset(tl_sec);
+	}
+
+	void Reset(const double tl_sec = DBL_MAX)
+	{
+    tmlim = tl_sec;
+    tpstart = clock::now();
+	}
+
+	double Elapsed() const
+	{
+		using namespace std::chrono;
+		return duration_cast<duration<double>>(clock::now() - tpstart).count();
+	}
+  
+	bool TimedOut() const { return Elapsed() >= tmlim; }
+};
